1bac68968660e5bbddd2ea7f04c87efa85161668
CENTRAL DE OFERTAS <centraldeofertas.ofical@gmail.com>
2026-04-06 22:24:03 -0300

    Refactor selectors for product cards and details

--- scraper/amazon/selectors.js
+++ scraper/amazon/selectors.js
@@ -1,63 +1,49 @@
 const selectors = {
   productCards: [
-    'div[data-component-type="s-search-result"]',
-    'div[data-component-type="s-impression-logger"][data-asin]:not([data-asin=""])',
-    'div[data-testid="deal-card"]',
-    'div[data-component-type="dealContent"]',
-    'div.DealCard-module__card',
-    'div[class*="DealCard"]',
-    'div[data-asin]:not([data-asin=""])[class*="deal"]',
-    'li[data-asin]:not([data-asin=""])'
+    'div[data-testid="product-card"]',
+    'div[data-component-type="s-search-result"]'
   ],
 
   title: [
+    'a[data-testid="product-card-link"] img[alt]',
     '[data-cy="title-recipe"] span',
     'h2 a span',
     'h2 span.a-size-medium',
     'h2 span.a-size-base-plus',
-    'h2 span',
-    'span[data-testid="deal-title"]',
-    'a[data-testid="product-title"]',
-    'span.DealCard-module__title',
-    'span[class*="dealTitle"]',
-    'span[class*="DealTitle"]'
+    'h2 span'
   ],
 
   link: [
+    'a[data-testid="product-card-link"][href]',
     '[data-cy="title-recipe"] a[href]',
     'h2 a[href]',
     'a.a-link-normal.s-no-outline[href]',
-    'a[data-testid="product-link"][href]',
-    'a[href*="/dp/"]',
-    'a[href*="/gp/product/"]'
+    'a[href*="/dp/"]'
   ],
 
   image: [
+    'img[data-testid="product-image"][src]',
     'img.s-image[src]',
     '.s-product-image-container img[src]',
-    'img[data-image-index][src]',
-    'img[data-testid="product-image"][src]',
-    'div[class*="DealCard"] img[src]',
-    'img[class*="dealImage"][src]'
+    'a[data-testid="product-card-link"] img[src]'
   ],
 
   priceOffscreen: [
     '[data-cy="price-recipe"] .a-price .a-offscreen',
     '.a-price:not(.a-text-price) .a-offscreen',
-    'span[data-testid="deal-price"] .a-offscreen',
-    'div[class*="DealCard"] .a-price .a-offscreen'
+    'div[data-testid="product-card"] .a-price .a-offscreen'
   ],
 
   priceWhole: [
     '[data-cy="price-recipe"] .a-price-whole',
     '.a-price:not(.a-text-price) .a-price-whole',
-    'span[data-testid="deal-price"] .a-price-whole'
+    'div[data-testid="product-card"] .a-price-whole'
   ],
 
   priceFraction: [
     '[data-cy="price-recipe"] .a-price-fraction',
     '.a-price:not(.a-text-price) .a-price-fraction',
-    'span[data-testid="deal-price"] .a-price-fraction'
+    'div[data-testid="product-card"] .a-price-fraction'
   ],
 
   oldPrice: [
